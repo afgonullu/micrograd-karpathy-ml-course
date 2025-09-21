1f2b56c23d0c8607bc2a41b48b8e9a06efb9ec2a
Abdullah Faruk G <afgonullu@gmail.com>
2025-09-21 10:45:52 +0200

    build topology and automatic back propagation

--- engine.py
+++ engine.py
@@ -1,9 +1,22 @@
 import math
 
+def build_topological_sort(root):
+	visited = set()
+	stack = []
+	def build(v):
+		if v not in visited:
+			visited.add(v)
+			for child in v._prev:
+				build(child)
+			stack.append(v)
+	build(root)
+	return stack
+
 class Value:
 		def __init__(self, data, _children=(), _op='', label=''):
 			self.data = data
 			self._prev = set(_children)
+			self._backward = lambda: None
 			self._op = _op
 			self.label = label
 			self.grad = 0.0
@@ -13,14 +26,30 @@ class Value:
 
 		def __add__(self, other):
 			out = Value(self.data + other.data, (self, other), '+')
-			return out
 
+			def _backward():
+				self.grad = 1.0 * out.grad
+				other.grad = 1.0 * out.grad
+
+			out._backward = _backward
+			return out
 		def __mul__(self, other):
 			out = Value(self.data * other.data, (self, other), '*')
+
+			def _backward():
+				self.grad = other.data * out.grad
+				other.grad = self.data * out.grad
+
+			out._backward = _backward
 			return out
 
 		def tanh(self):
 			x = self.data
 			t = (math.exp(2* x) - 1) / (math.exp(2* x) + 1)
 			out = Value(t, (self,), 'tanh')
+
+			def _backward():
+				self.grad = (1 - t**2) * out.grad
+
+			out._backward = _backward
 			return out
