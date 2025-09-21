af035125b548d2b0f59b969e9111a10ace59aa0d
Abdullah Faruk G <afgonullu@gmail.com>
2025-09-21 11:46:37 +0200

    all operations capability and explicit tanh

--- engine.py
+++ engine.py
@@ -25,6 +25,7 @@ class Value:
 			return f"Value(data={self.data})"
 
 		def __add__(self, other):
+			other = other if isinstance(other, Value) else Value(other)
 			out = Value(self.data + other.data, (self, other), '+')
 
 			def _backward():
@@ -33,7 +34,12 @@ class Value:
 
 			out._backward = _backward
 			return out
+
+		def __radd__(self, other): # other + self
+			return self + other
+
 		def __mul__(self, other):
+			other = other if isinstance(other, Value) else Value(other)
 			out = Value(self.data * other.data, (self, other), '*')
 
 			def _backward():
@@ -43,6 +49,33 @@ class Value:
 			out._backward = _backward
 			return out
 
+		def __rmul__(self, other): # other * self
+			return self * other
+
+		def __pow__(self, other):
+			assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+			out = Value(self.data**other, (self,), f'**{other}')
+
+			def _backward():
+				self.grad += other * (self.data**(other-1)) * out.grad
+			out._backward = _backward
+			return out
+
+		def __truediv__(self, other):
+			return self * (other**-1)
+
+		def __rtruediv__(self, other): # other / self
+			return other * (self**-1)
+
+		def __neg__(self):
+			return self * -1
+
+		def __sub__(self, other):
+			return self + (-other)
+
+		def __rsub__(self, other): # other - self
+			return other + (-self)
+
 		def tanh(self):
 			x = self.data
 			t = (math.exp(2* x) - 1) / (math.exp(2* x) + 1)
@@ -50,6 +83,16 @@ class Value:
 
 			def _backward():
 				self.grad += (1 - t**2) * out.grad
+
+			out._backward = _backward
+			return out
+
+		def exp(self):
+			x = self.data
+			out = Value(math.exp(x), (self,), 'exp')
+
+			def _backward():
+				self.grad += out.data * out.grad
 
 			out._backward = _backward
 			return out
